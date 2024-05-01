52aa0bc8336d9f49f916e3b57d01d567ad269c84
BrianKellyCS <bkelly8946@gmail.com>
2024-05-01 10:47:15 -0700

    update graph with correct data

--- comps/Data.js
+++ comps/Data.js
@@ -1,167 +1,113 @@
 import React, { useState, useEffect } from "react";
-import { Bar, Line } from "react-chartjs-2";
-import Chart from "chart.js/auto";
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale
+} from 'chart.js';
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale,
+
+);
+import 'chartjs-adapter-date-fns';
 import styles from "../styles/Data.module.css";
 
 function Data() {
   const colors = [
-    "rgba(255, 99, 132, 0.5)", // red
-    "rgba(54, 162, 235, 0.5)", // blue
-    "rgba(255, 206, 86, 0.5)", // yellow
-    "rgba(75, 192, 192, 0.5)", // green
+    "rgba(255, 99, 132, 0.5)",  // red
+    "rgba(54, 162, 235, 0.5)",  // blue
+    "rgba(255, 206, 86, 0.5)",  // yellow
+    "rgba(75, 192, 192, 0.5)",  // green
     "rgba(153, 102, 255, 0.5)", // purple
-    "rgba(255, 159, 64, 0.5)", // orange
+    "rgba(255, 159, 64, 0.5)"   // orange
   ];
   const chartOptions = {
-    scales: { y: { beginAtZero: true } },
+    scales: {
+      y: {
+        beginAtZero: true
+      },
+      x: {
+        type: 'time',
+        time: {
+          unit: 'day',
+          tooltipFormat: 'MM/dd/yyyy HH:mm',
+          displayFormats: {
+            hour: 'HH:mm',
+            day: 'MM/dd/yyyy'
+          }
+        },
+        ticks: {
+          source: 'auto',
+          autoSkip: true,
+          maxTicksLimit: 20
+        }
+      }
+    },
     animation: {
-      duration: 5, // general animation time
+      duration: 5
     },
     hover: {
-      animationDuration: 5, // duration of animations when hovering an item
+      animationDuration: 5
     },
-    responsiveAnimationDuration: 0, // animation duration after a resize
+    responsiveAnimationDuration: 0
   };
+  
   const [mockData, setHistorical] = useState([]);
-  const [activeTab, setActiveTab] = useState("fillLevels");
+  const [activeTab, setActiveTab] = useState('fillLevels');
+  const [fillLevelsOverTime, setFillLevelsOverTime] = useState({
+    labels: [],
+    datasets: []
+  });
+  const [lastSavedTimes, setLastSavedTimes] = useState({});
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  console.log(mockData);
 
-  const getHistorical = () => {
-    fetch(`${process.env.NEXT_PUBLIC_SERVER_LINK}/get-historical-for-routes`, {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      credentials: "include",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status) {
-          setHistorical(data.data);
-        } else {
-          console.log(data.msg);
-        }
-      })
-      .catch((e) => console.log(e));
-  };
 
   useEffect(() => {
+    const getHistorical = () => {
+      fetch(`${process.env.NEXT_PUBLIC_SERVER_LINK}/get-historical-for-routes`, {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      })
+        .then(res => res.json())
+        .then(data => {
+          if (data.status) {
+            setHistorical(data.data);
+
+            // Calculate start and end dates based on the fetched data
+            const dates = data.data.map(item => new Date(item.saved_time));
+            const minDate = dates.length ? new Date(Math.min(...dates)) : new Date();
+            const maxDate = dates.length ? new Date(Math.max(...dates)) : new Date();
+
+            setStartDate(minDate.toISOString().split('T')[0]);
+            setEndDate(maxDate.toISOString().split('T')[0]);
+          } else {
+            console.log(data.msg);
+          }
+        })
+        .catch(e => console.log(e));
+    };
+
     getHistorical();
   }, []);
 
-  // function generateDynamicMockBins() {
-  //   const currentDateTime = new Date();
-  //   const startDateTime = new Date(currentDateTime.getTime() - 24 * 3600000);
-  //   const mockBinsData = [];
-  //   const binSpecifications = [
-  //       { uniqueId: 56, fillRate: 1, startLevel: 9, hours: 6 },
-  //       { uniqueId: 57, fillRate: 2, startLevel: 8, hours: 6 },
-  //       { uniqueId: 58, fillRate: 9, startLevel: 10, hours: 6 },
-  //       { uniqueId: 59, fillRate: 0.1, startLevel: 6, hours: 6 },
-  //   ];
-
-  //   binSpecifications.forEach(spec => {
-  //       for (let i = 0; i <= spec.hours; i++) {
-  //           let date = new Date(startDateTime.getTime() + i * 3600000);
-  //           let level = Math.min(spec.startLevel + i * spec.fillRate, 75);
-  //           mockBinsData.push({ unique_id: spec.uniqueId, level_in_percents: level, saved_time: date.toISOString() });
-  //       }
-  //   });
-  //   return mockBinsData;
-  // }
-  // let mockData = generateDynamicMockBins();
-
-  useEffect(() => {
-    updateChartData();
-  }, [mockData]);
-
-  const updateChartData = () => {
-    // Group data by unique_id to calculate average fill levels per device
-    const groupedByDevice = mockData.reduce((acc, item) => {
-      if (!acc[item.unique_id]) {
-        acc[item.unique_id] = [];
-      }
-      acc[item.unique_id].push(item);
-      return acc;
-    }, {});
-
-    // Create datasets for each unique device using a predefined color set
-    const datasets = Object.keys(groupedByDevice).map((unique_id, index) => {
-      const data = groupedByDevice[unique_id];
-      const colorIndex = index % colors.length; // Cycle through colors if more devices than colors
-      return {
-        label: `Device ${unique_id}`,
-        data: data.map((item) => item.level_in_percents),
-        borderColor: colors[colorIndex],
-        backgroundColor: colors[colorIndex],
-        fill: false,
-        lineTension: 0.1,
-      };
-    });
-
-    setFillLevelsOverTime({
-      labels: mockData.map((data) =>
-        new Date(data.saved_time).toLocaleString()
-      ),
-      datasets,
-    });
-
-    // Update other charts as necessary
-    const frequencyData = calculateFillEvents(mockData);
-    setMostFrequentlyFilledBins({
-      labels: Object.keys(frequencyData),
-      datasets: [
-        {
-          label: "Number of Fill Cycles",
-          data: Object.values(frequencyData),
-          backgroundColor: "rgba(255, 205, 86, 0.2)",
-          borderColor: "rgba(255, 205, 86, 1)",
-        },
-      ],
-    });
-
-    const pingCounts = mockData.reduce((acc, data) => {
-      acc[data.unique_id] = (acc[data.unique_id] || 0) + 1;
-      return acc;
-    }, {});
-
-    setPingsPerDevice({
-      labels: Object.keys(pingCounts),
-      datasets: [
-        {
-          label: "Number of Pings",
-          data: Object.values(pingCounts),
-          backgroundColor: "rgba(132, 99, 255, 0.2)",
-          borderColor: "rgba(132, 99, 255, 1)",
-        },
-      ],
-    });
-  };
-
-  const calculateFillEvents = (data) => {
-    const events = {};
-    data.forEach((point, index) => {
-      if (point.level_in_percents < 10) {
-        const next = data[index + 1];
-        if (next && next.level_in_percents >= 75) {
-          events[next.unique_id] = (events[next.unique_id] || 0) + 1;
-        }
-      }
-    });
-    return events;
-  };
-
-  const [fillLevelsOverTime, setFillLevelsOverTime] = useState({
-    labels: [],
-    datasets: [],
-  });
-
-  const [mostFrequentlyFilledBins, setMostFrequentlyFilledBins] = useState({
-    labels: [],
-    datasets: [],
-  });
-
-  const [pingsPerDevice, setPingsPerDevice] = useState({
-    labels: [],
-    datasets: [],
-  });
 
   const clearHistorical = () => {
     fetch(`${process.env.NEXT_PUBLIC_SERVER_LINK}/clear-all-historical`, {
@@ -178,47 +124,92 @@ function Data() {
         }
       });
   };
+ 
+  useEffect(() => {
+    updateChartData();
+  }, [mockData, startDate, endDate]);
+
+
+
+
+  /////////////////////////////////////
+  const updateChartData = () => {
+
+    const filteredData = mockData.filter(item => {
+      const itemDate = new Date(item.saved_time);
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+      return itemDate >= start && itemDate <= end;
+    });
+    console.log(filteredData);
+    const groupedByDevice = filteredData.reduce((acc, item) => {
+      if (!acc[item.unique_id]) {
+        acc[item.unique_id] = { data: [], lastSavedTime: null };
+      }
+      // Create a new Date object from the saved_time
+      const itemDate = new Date(item.saved_time);
+      // Calculate timezone offset in milliseconds
+      const adjust = itemDate.getTimezoneOffset() * 60 * 1000;
+      // Adjust the date by the timezone offset
+      const dateToSave = new Date(itemDate.getTime() + adjust);
+  
+      acc[item.unique_id].data.push({
+        ...item,
+        saved_time: dateToSave // Store the adjusted date back in the item
+      });
+  
+      // Check and update the last saved time for this device
+      if (!acc[item.unique_id].lastSavedTime || dateToSave > acc[item.unique_id].lastSavedTime) {
+        acc[item.unique_id].lastSavedTime = dateToSave;
+      }
+      return acc;
+    }, {});
+  
+    const datasets = [];
+    const lastTimes = {};
+  
+    for (const [unique_id, { data, lastSavedTime }] of Object.entries(groupedByDevice)) {
+      const colorIndex = unique_id % colors.length;
+      datasets.push({
+        label: `Device ${unique_id}`,
+        data: data.map(item => ({
+          x: item.saved_time, // Use the adjusted timestamp
+          y: item.level_in_percents // Data value
+        })),
+        borderColor: colors[colorIndex],
+        backgroundColor: colors[colorIndex],
+        fill: false,
+        lineTension: 0.1
+      });
+      // Store the last saved time for display
+      lastTimes[unique_id] = lastSavedTime.toLocaleString();
+    }
+  
+    setFillLevelsOverTime({
+      datasets
+    });
+    setLastSavedTimes(lastTimes); // Update state with the last saved times
+  };
 
   return (
     <div className={styles.analytics_container}>
       <div className={styles.date_range_selector}>
-        <button onClick={() => setActiveTab("fillLevels")}>
-          Fill Levels Over Time
-        </button>
-        <button onClick={() => setActiveTab("frequentFills")}>
-          Most Frequently Filled Bins
-        </button>
-        <button onClick={() => setActiveTab("pings")}>
-          Number of Pings per Device
-        </button>
+        <input type="date" value={startDate} onChange={e => setStartDate(e.target.value)} />
+        <input type="date" value={endDate} onChange={e => setEndDate(e.target.value)} />
       </div>
       <button className={styles.red_button} onClick={clearHistorical}>
         Clear Historical Data
       </button>
       <div className={styles.chart_container}>
-        {activeTab === "fillLevels" && (
+        {activeTab === 'fillLevels' && (
           <div className={styles.chart}>
             <h2>Average Fill Levels Over Time</h2>
             <Line data={fillLevelsOverTime} options={chartOptions} />
-          </div>
-        )}
-
-        {activeTab === "frequentFills" && (
-          <div className={styles.chart}>
-            <h2>Most Frequently Filled Bins</h2>
-            <Bar data={mostFrequentlyFilledBins} options={chartOptions} />
-          </div>
-        )}
-
-        {activeTab === "pings" && (
-          <div className={styles.chart}>
-            <h2>Number of Pings per Device</h2>
-            <Bar data={pingsPerDevice} options={chartOptions} />
           </div>
         )}
       </div>
     </div>
   );
 }
-
 export default Data;
+
